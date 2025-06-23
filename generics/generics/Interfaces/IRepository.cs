b7ffb1c9e595014a716742d366e1cf0bc8a4d5b8
Timur0919 <93837190+Timur0919@users.noreply.github.com>
2025-06-23 17:38:10 +0300

    Done

--- generics/generics/Interfaces/IRepository.cs
+++ generics/generics/Interfaces/IRepository.cs
@@ -1,10 +1,24 @@
-﻿namespace generics.Interfaces
+namespace generics.Interfaces
 {
     public interface IRepository<TEntity, TKey>
+        where TEntity : class, new()
+        where TKey : struct
     {
         void Add(TKey id, TEntity entity);
         TEntity Get(TKey id);
         IEnumerable<TEntity> GetAll();
+        void Remove(TKey id);
+    }
+
+    public interface IReadOnlyRepository<out TEntity, in TKey>
+    {
+        TEntity Get(TKey id);
+        IEnumerable<TEntity> GetAll();
+    }
+
+    public interface IWriteRepository<in TEntity, in TKey>
+    {
+        void Add(TEntity entity);
         void Remove(TKey id);
     }
 }
